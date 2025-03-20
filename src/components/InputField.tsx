8b1a2f9a812e50405a19b6f8d16f4893eaa46768
ianmsfvenom <iangamer296@gmail.com>
2025-03-20 10:36:00 -0300

    first commit

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -3,7 +3,9 @@ import styles from '../styles/InputField.module.css';
 export default function InputField() {
     return (
         <div className={styles.inputField}>
-            
+            <div className={styles.inputBox}>
+                
+            </div>
         </div>
     );
 }
